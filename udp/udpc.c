c19a8499f0f9f2d8decf35437b2e45a79b99e907
Alan Joy <147591098+Alan9072@users.noreply.github.com>
2025-04-08 18:09:43 +0530

    Update udpc.c

--- udp/udpc.c
+++ udp/udpc.c
@@ -1,29 +1,55 @@
-#include<stdio.h>
-#include<string.h>
-#include<sys/socket.h>
-#include<stdlib.h>
-#include<netdb.h>
+#include <arpa/inet.h> // inet_addr()
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h> // bzero()
+#include <sys/socket.h>
+#include <unistd.h> // read(), write(), close()
 
-int main(int argc,char* argv[])
-{
-    struct sockaddr_in server,client;
-    if(argc!=3)
-    printf("Input format not correct");
+#define PORT 4000
+#define MAX 1024
 
-    int sockfd=socket(AF_INET,SOCK_DGRAM,0);
-    if(sockfd==-1)
-    printf("Error in socket();");
+int main() {
+    int sockfd, n, len;
+    char buf[MAX + 1];  // +1 for null termination
+    struct sockaddr_in server;
 
-    server.sin_family=AF_INET;
-    server.sin_addr.s_addr=INADDR_ANY;
-    server.sin_port=htons(atoi(argv[2]));
+    // Create UDP socket
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+        printf("Error in creating socket\n");
+    else
+        printf("Socket successfully created\n");
 
-    char buffer[100];
-    printf("Enter a message to be sent to server");
-    fgets(buffer,100,stdin);
+    // Zero out server struct
+    bzero(&server, sizeof(server));
 
-    if(sendto(sockfd,buffer,sizeof(buffer),0,(struct sockaddr*)&server,sizeof(server))<0)
-    printf("Error in sendto");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
 
+    len = sizeof(server);  // Set len before loop
+
+    printf("Connecting to server...\n");
+
+    while (1) {
+        printf("Message to be sent: ");
+        scanf("%s", buf);
+
+        sendto(sockfd, buf, sizeof(buf), 0, (struct sockaddr*)&server, len);
+
+        if (strcmp(buf, "quit") == 0)
+            break;
+
+        printf("Message sent. Waiting for the response...\n");
+
+        n = recvfrom(sockfd, buf, sizeof(buf), 0, (struct sockaddr*)&server, &len);
+        buf[n] = '\0';
+
+        if (n > 1)
+            printf("Received: %s\n", buf);
+    }
+
+    close(sockfd);
     return 0;
 }
